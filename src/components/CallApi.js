f64c1791d5327d10b7b2b7854b2f7b2de3cc0e9e
Israel Fonseca Zárate <ifonsecap351@gmail.com>
2025-06-06 21:41:36 -0600

    corrected api call

--- src/components/CallApi.js
+++ src/components/CallApi.js
@@ -1,10 +1,14 @@
 export const apiCall = async (cityname) => {
-    const url = `/.netlify/functions/fetchWeatherData?city=${cityname}`;
+    const url = `/.netlify/functions/WeatherApi?city=${cityname}`;
     try {
         const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Error ${response.status}: ${response.statusText}`);
+         }
         const data = await response.json();
         return data;
-    } catch (err) {
-        console.log(err);
-    }
+  } catch (err) {
+    console.error('API call error:', err);
+    return null;
+  }
 }
